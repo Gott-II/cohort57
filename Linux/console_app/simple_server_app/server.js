e98348324fc9cc1d4129259ff74d3be3a26e678f
Alexander_Goette <gott_ii@gmx.de>
2025-08-15 10:33:17 +0200

    Docker

---
+++ Linux/console_app/simple_server_app/server.js
@@ -0,0 +1,26 @@
+/**
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello from Docker server!\n');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+}); */
+
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ message: "Hello from Docker server!\n" }));
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
